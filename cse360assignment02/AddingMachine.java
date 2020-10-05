07e8fe7de6d8e0362ba1edf82761dcb4bcebe387
vtle5 <>
2020-10-05 03:23:26 -0700

    Commiting package

---
+++ cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+/**
+ * This package holds a AddingMachine class which
+ * will be able to add and subtract
+ *
+ * @author  Vincent Le
+ * @date    October 5, 2020
+ */
+
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    /**
+     * This is the AddingMachine Constructor
+     * It sets the total variable to 0
+     */
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    /**
+     * This method returns the total variable
+     * @return int This returns total
+     */
+    public int getTotal () {
+        return 0;
+    }
+
+    /**
+     * This method adds to the total variable
+     * @param value This variable is added to the total variable
+     */
+    public void add (int value) {
+    }
+
+    /**
+     * This method subtracts from the total variable
+     * @param value This variable subtracts from the total variable
+     */
+    public void subtract (int value) {
+    }
+
+    /**
+     * This method returns the history of calculations in string format
+     * @return string Returns the history
+     */
+    public String toString () {
+        return "";
+    }
+
+    /**
+     * This method clears the history of calculations
+     */
+    public void clear() {
+    }
+}
